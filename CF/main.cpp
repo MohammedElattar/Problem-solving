15d57c70d9ab796cbe3bcc8915e1f369ee4d72ca
Mohamed Attar <mohammedattar0100020@gmail.com>
2022-12-20 13:15:31 +0200

    initial commit

---
+++ CF/main.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+    cout << "Hello from the other side";
+}
+#define fst                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+int main(){
+    fst;
+    solve();
+}
